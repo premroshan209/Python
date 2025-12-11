e2e8f75ae3ad30e63277f7210cb34ac0e8a2a1eb
docddr17-lgtm <docddr17.lgtm@gmail.com>
2025-12-11 21:28:42 +0530

    Chapter 1: Variables and Data Types

---
+++ chapter1_variables_and_dataTypes/area.py
@@ -0,0 +1,2 @@
+side = int(input("Enter side of a square : "))
+print("area of square = ",side ** 2)
---
+++ chapter1_variables_and_dataTypes/first.py
@@ -0,0 +1,8 @@
+name = input("Enter your Name: ")    #Only sutable for string input, not for integer or othere numbers
+print("Hii ", name)
+
+
+age = int(input("Enter your age: "))  #for integer inputs
+print("age : ",age)
+
+hight = float(input("Enter your hight: "))  #for float inputs
